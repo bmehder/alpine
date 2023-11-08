f0bf7403a601f30a3bb597b311586c2a006c76f1
bmehder <bmehder@gmail.com>
2023-11-07 19:36:20 -0500

    cleaned up loading scripts

---
+++ tabs-data.js
@@ -0,0 +1,17 @@
+const tabs = [
+  {
+    name: 'Tab 1',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis commodi aliquid, laudantium aspernatur modi dolorem consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis commodi aliquid, laudantium aspernatur modi dolorem consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis commodi aliquid, laudantium aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    name: 'Tab 2',
+    content:
+      'Dolor sit amet consectetur adipisicing elit. Debitis commodi aliquid, modi dolorem consequatur. Debitis commodi aliquid, modi dolorem consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sit amet consectetur adipisicing elit. Debitis commodi aliquid, modi dolorem consequatur. Debitis commodi aliquid, modi dolorem consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    name: 'Tab 3',
+    content:
+      'Debitis commodi aliquid, modi dolorem consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sit amet consectetur adipisicing elit. Debitis commodi aliquid, modi dolorem consequatur. Debitis commodi aliquid, modi dolorem consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sit amet consectetur adipisicing elit. Debitis commodi aliquid, modi dolorem consequatur.',
+  },
+]
